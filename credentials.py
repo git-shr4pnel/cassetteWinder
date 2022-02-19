63afefb2862d56c94447924b681f369cc3c87abf
shr4ppy <54712289+shr4ppy@users.noreply.github.com>
2022-02-19 23:47:57 +0000

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-SPOTIPY_CLIENT_ID =
-SPOTIPY_CLIENT_SECRET = 
+SPOTIPY_CLIENT_ID = ""
+SPOTIPY_CLIENT_SECRET = ""
 # my recommended callback uri: set your own in the spotify developer portal
 SPOTIPY_CALLBACK_URI = "http://localhost:8080/callback" 
